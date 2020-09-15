6c7e40377346646adfec156a742301ba45cf1231
bmfukushima <bmfukushima@gmail.com>
2020-09-15 09:07:01 -0700

    fixed import erros

--- cgwidgets/widgets/__init__.py
+++ cgwidgets/widgets/__init__.py
@@ -2,6 +2,6 @@ from .LibraryWidget import LibraryWidget as LibraryWidget
 from .ColorWidget import ColorWidget as ColorWidget
 from .LadderWidget import LadderWidget as LadderWidget
 from .BaseTansuWidget import BaseTansuWidget
-from .TabTansuWidget import TabTansuWidget
+from .TansuTabWidget import TansuTabWidget
 
 from .__utils__ import *
